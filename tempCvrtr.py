c7d115fd0a2907e411f2cf21a574a170c138f37e
Katte <tacocatytreal@gmail.com>
2022-03-22 23:54:19 -0700

    firdt commit

---
+++ tempCvrtr.py
@@ -0,0 +1,7 @@
+import disnake
+from disnake.ext import commands
+
+
+client = commands.Bot(command_prefix="b|", test_guilds=[951923779859271711], intents=disnake.Intents.default())
+
+client.run("ODg1MzExMzg2MjA3NTI2OTMy.YTlMeg.a0lXA55qTmiUoWjyiheJ0FqOKiI")
