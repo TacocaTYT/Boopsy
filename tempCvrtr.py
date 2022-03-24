650a6096d0fb4cd5788a0f3e384b2c7f0d6cd02b
Katte <tacocatytreal@gmail.com>
2022-03-23 23:30:05 -0700

    ahhh

--- tempCvrtr.py
+++ tempCvrtr.py
@@ -1,8 +1,3 @@
-import disnake
-from token import token
-from disnake.ext import commands
-
-
-client = commands.Bot(command_prefix="b|", test_guilds=[951923779859271711], intents=disnake.Intents.default())
-
-client.run(token)
+from RandomWordGenerator import RandomWord
+rw = RandomWord(max_word_size=20,constant_word_size=False)
+print(rw.generate())
