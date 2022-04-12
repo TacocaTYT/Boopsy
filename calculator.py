8ae6b664594905e647471a8493de58bcf4aa9520
Katte <37640938+TacocaTYT@users.noreply.github.com>
2022-04-12 13:05:44 -0700

    Delete calculator.py

--- calculator.py
+++
@@ -1,6 +0,0 @@
-while True:
-    try:
-        user_input = "0+" + input("Equation: ").replace("^","**").replace("x","*").replace("X","*")
-        if any(c.isalpha() or c.isspace() for c in user_input):print("Not a valid equation")
-        else:exec("print(" + user_input + ")")
-    except:print("ERROR - Stop breaking stuff")
